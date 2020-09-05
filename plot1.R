18cce6494d083adcbfbf9b0afe8e1a31890dd71d
Tetiana Gorbach <gorbachtetiana@gmail.com>
2020-09-05 12:53:34 +0200

    changed from reading the file locally to downloading zip-file, unzipping, and reading the file afterwards

--- plot1.R
+++ plot1.R
@@ -1,5 +1,14 @@
 # this file reads the data and constructs plot1.png
 library(tidyverse)
+# read data
+if (!file.exists("exdata_data_household_power_consumption.zip")) {
+        download.file(url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                      destfile="exdata_data_household_power_consumption.zip",
+                      method="curl")
+        unzip("exdata_data_household_power_consumption.zip")  
+}
+
+
 dat <- read.table("household_power_consumption.txt", sep=";", header=T, na.strings="?",
                   colClasses = c(rep("character",2), rep("numeric",7)))
 dat <- dat%>%filter(Date %in% c("1/2/2007", "2/2/2007"))
